f72b216cc40dfad54cba0af27e6777ebdb541895
Huy Tran <fun.round@gmail.com>
2018-08-10 11:40:54 -0700

    Quiz completion

--- components/Quiz.js
+++ components/Quiz.js
@@ -1,11 +1,101 @@
 import React, { Component } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+import { PRIMARY_LIGHT, PRIMARY_DARK, ON_PRIMARY, SECONDARY, ON_SECONDARY } from '../utils/colors';
 
 class Quiz extends Component {
+  static navigationOptions = ({ navigation }) => {
+    const { title } = navigation.state.params;
+
+    return {
+      title: `${title} Quiz`
+    };
+  }
+  state = {
+    questions: [],
+    ready: false,
+    showResults: false,
+    score: 0
+  }
+  componentDidMount() {
+    const { questions } = this.props.navigation.state.params;
+
+    this.setState(() => ({
+      questions: questions.map((question) => ({ ...question, answered: false, correct: null, viewAnswer: false })),
+      ready: true
+    }));
+  }
+  toggleCard(index) {
+    const { questions } = this.state;
+    questions[index].viewAnswer = questions[index].viewAnswer ? false: true;
+    this.setState(() => ({
+      questions
+    }));
+  }
+  answerCard(index, correct) {
+    const { questions } = this.state;
+    questions[index].correct = correct;
+    questions[index].answered = true;
+    const activeIndex = questions.findIndex((question) => { return question.answered === false; });
+
+    this.setState(() => ({
+      questions,
+      showResults: activeIndex === -1 ? true: false,
+      score: ((questions.filter((question) => (question.correct)).length/questions.length)*100).toFixed(2)
+    }));
+  }
+  resetQuiz() {
+    this.setState((state) => ({
+      questions: state.questions.map((question) => {
+        question.answered = false;
+        question.correct = null;
+        question.viewAnswer = false;
+        return question;
+      }),
+      showResults: false,
+      score: 0
+    }));
+  }
   render() {
+    const { ready, questions, showResults, score } = this.state;
+    const activeQuestion = questions.find((question) => { return question.answered === false; });
+    const activeIndex = questions.findIndex((question) => { return question.answered === false; });
+
     return (
       <View style={styles.container}>
-        <Text>Quiz</Text>
+        {ready === true && (
+          <View style={{ width: 400 }}>
+            {questions.length === 0 && (
+              <Text style={styles.header}>There are no questions yet in this quiz deck.</Text>
+            )}
+            {questions.length > 0 && (
+              <View>
+                {showResults && (
+                  <View>
+                    <Text style={styles.header}>You've completed the quiz with a score of {score}%!</Text>
+                    <TouchableOpacity style={styles.buttonReset} onPress={() => this.resetQuiz()}>
+                      <Text style={styles.buttonResetText}>Reset Quiz</Text>
+                    </TouchableOpacity>
+                  </View>
+                )}
+                {showResults === false && (
+                  <View>
+                    <Text style={styles.header}>{activeQuestion.viewAnswer ? activeQuestion.answer : activeQuestion.question}</Text>
+                    <TouchableOpacity style={styles.button} onPress={() => this.toggleCard(activeIndex)}>
+                      <Text style={styles.buttonText}>{activeQuestion.viewAnswer ? 'View Question' : 'View Answer'}</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={styles.buttonCorrect} onPress={() => this.answerCard(activeIndex, true)}>
+                      <Text style={styles.buttonCorrectText}>Correct</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={styles.buttonIncorrect} onPress={() => this.answerCard(activeIndex, false)}>
+                      <Text style={styles.buttonIncorrectText}>Incorrect</Text>
+                    </TouchableOpacity>
+                    <Text style={styles.questionCount}>{activeIndex+1} of {questions.length} cards.</Text>
+                  </View>
+                )}
+              </View>
+            )}
+          </View>
+        )}
       </View>
     );
   }
@@ -13,10 +103,66 @@ class Quiz extends Component {
 
 const styles = StyleSheet.create({
   container: {
-    // flex: 1,
-    // backgroundColor: '#FFF',
-    padding: 15
-  }
+    flex: 1,
+    backgroundColor: '#FFF',
+    padding: 15,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  header: {
+    fontSize: 36,
+    textAlign: 'center'
+  },
+  questionCount: {
+    color: PRIMARY_LIGHT,
+    textAlign: 'center',
+    fontSize: 24
+  },
+  button: {
+    borderColor: PRIMARY_DARK,
+    borderWidth: 1,
+    margin: 5,
+    borderRadius: 5,
+    padding: 10
+  },
+  buttonText: {
+    fontSize: 18,
+    textAlign: 'center',
+    color: PRIMARY_DARK
+  },
+  buttonCorrect: {
+    backgroundColor: 'green',
+    margin: 5,
+    borderRadius: 5,
+    padding: 10
+  },
+  buttonCorrectText: {
+    fontSize: 18,
+    textAlign: 'center',
+    color: '#FFF'
+  },
+  buttonIncorrect: {
+    backgroundColor: 'red',
+    margin: 5,
+    borderRadius: 5,
+    padding: 10
+  },
+  buttonIncorrectText: {
+    fontSize: 18,
+    textAlign: 'center',
+    color: '#FFF'
+  },
+  buttonReset: {
+    backgroundColor: PRIMARY_DARK,
+    margin: 5,
+    borderRadius: 5,
+    padding: 10
+  },
+  buttonResetText: {
+    fontSize: 18,
+    textAlign: 'center',
+    color: ON_PRIMARY
+  },
 });
 
 export default Quiz;
